8979cbe9caeeefe242faf708bc98f191151e15f7
GabrielFelix23 <srgfelix123@gmail.com>
2020-10-07 15:56:16 -0300

    Tela home pronta

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import firebase from '../../firebase'
+import './home.css'
 
 class Home extends React.Component{
+  state = {
+    posts: []
+  }
+
+  componentDidMount(){
+    firebase.app.ref('posts').on('value', (snapshot) => {
+      let state = this.state
+      state.posts = []
+
+      snapshot.forEach((childItem) => {
+        state.posts.push({
+          key: childItem.key,
+          autor: childItem.val().autor,
+          descricao: childItem.val().descricao,
+          image: childItem.val().image
+        })
+        this.setState(state)
+      })
+    })
+  }
+
   render() {
     return (
-      <div>
-        <h1>Tela Home</h1>
-      </div>
+      <section id="posts">
+        {this.state.posts.map((posts) => {
+          return(   
+            <article key={posts.key}>
+                <p className="titulo">Autor: {posts.autor}</p>
+                <img src={posts.image} alt="Capa do post"/>
+                <p className="descricao">Descrição: {posts.descricao}</p>
+            </article>  
+          )
+        })}
+      </section>
     );
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+import 'firebase/storage'
+
+let firebaseConfig = {
+    apiKey: "AIzaSyAwMlHK3aMbSc9uiXgd6_jhoA9n8B8t3-U",
+    authDomain: "devblog-964d8.firebaseapp.com",
+    databaseURL: "https://devblog-964d8.firebaseio.com",
+    projectId: "devblog-964d8",
+    storageBucket: "devblog-964d8.appspot.com",
+    messagingSenderId: "179892401896",
+    appId: "1:179892401896:web:b397003e6fe154f33e7b5d"
+}
+
+class Firebase{
+    constructor(){
+        if(!firebase.apps.length){
+            firebase.initializeApp(firebaseConfig)
+        }
+        this.app = firebase.database()
+    }
+}
+
+export default new Firebase()
