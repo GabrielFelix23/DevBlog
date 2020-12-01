012fe61bbc851715024e5af2e313f377083e901f
GabrielFelix23 <srgfelix123@gmail.com>
2020-11-30 21:23:53 -0300

    Mais ajustes na foto de perfil

--- src/components/Perfil/EditPerfil/index.js
+++ src/components/Perfil/EditPerfil/index.js
@@ -10,15 +10,11 @@ class EditPerfil extends React.Component{
         super(props)
         this.state = {
             nome: localStorage.nome,
-            email: '',
-            senha: '',
             imagem: null,
             url: '',
             progress: 0
         }
         this.editar = this.editar.bind(this)
-        this.fotoUser = this.fotoUser.bind(this)
-        this.salvarFoto = this.salvarFoto.bind(this)
     }
 
     async editar(e){
@@ -29,50 +25,8 @@ class EditPerfil extends React.Component{
         await firebase.app.ref('usuario').child(uid).set({
             nome: nome,
         })
-
-        firebase.app.ref('fotoUser').child(uid).set({
-            fotoPerfil: this.state.url
-        }).then(() => {
+        .then(() => {
             this.props.history.replace("/dashboard")
-        })
-    }
-
-    fotoUser = async (e) => { 
-        if(e.target.files[0]){
-            const image = e.target.files[0]
-            if(image.type === 'image/png' || image.type === 'image/jpeg' || image.type === 'image/jpg'){
-                await this.setState({imagem: image})
-                await this.salvarFoto()
-            }
-            else{
-                alert("Apenas fotos png, jpeg ou jpg.")
-                this.setState({imagem: null})
-                return null
-            }
-        }
-    }
-
-    salvarFoto = async () => {
-        const {imagem} = this.state
-        const uid = firebase.Uid()
-    
-        const salvandoImg = firebase.storage.ref(`fotoDePerfil/${uid}/${imagem.name}`).put(imagem)
-        await salvandoImg.on('state_changed',(snapshot) => {
-            const progress = Math.round(
-                (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-              )
-              this.setState({progress: progress})
-        }, (erro) => {
-            console.log("Erro: " + erro)
-            
-        }, () => {
-            firebase.storage.ref(`fotoDePerfil/${uid}`)
-            .child(imagem.name).getDownloadURL()
-            .then((url) => {
-                this.setState({
-                    url: url
-                })
-            })
         })
     }
 
@@ -89,14 +43,12 @@ class EditPerfil extends React.Component{
                 <div className="container">
                     <h1>Foto de perfil: </h1>
 
-                    <form onSubmit={this.editar}>
-                        <input type="file" onChange={this.fotoUser}/>
-                        {this.state.url !== '' ? 
-                            <img src={this.state.url} alt="Foto de perfil" width="150" height="150"/>
-                            :
-                            <progress value={this.state.progress} max="100"/>
-                        }<br/>
-                        <label>Nome: </label><br/>
+                    <form onSubmit={this.editar}>  
+                        <div className="containerFotoPerfil">
+                            <img src={semFoto} className="semFoto" alt="foto de perfil"/>
+                            <Link to={'/fotoPerfil'} className="buttonFotoPerfil">Foto de Perfil</Link>
+                        </div>          
+                        <label>Nome: </label>
                         <input type="text" placeholder="Gabriel..."
                             value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})}/><br/>                        
                         <div className="containerButton">
